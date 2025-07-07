dfa8c5bb7530fb9abe2a20440c086dc6db5df5fe
Devrashab <152000702+Devrashab@users.noreply.github.com>
2025-07-07 07:11:13 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; //treat All JS code  as newer version 
+
+// alert(3+3) // we are using Nodejs, not a brower 
+
+console.log(3+3)
+
+console.log("Imran");
+
+ let name ="imran"
+ let age = 20
+let isLoggedIn = false
+
+
+//number => 2 to power 53 
+// bigint 
+// string => ""
+//boolean => true/false
+//null => standalone value 
+//undifend =>
+//symbol => unique
+
+
+//object 
+
+
+
+
+console.log(typeof"imran"); // string
+console.log(typeof age ); //number 
+console.log(typeof null); //object
+console.log(typeof undefined); //undefind
---
+++ basics/veriables.js
@@ -0,0 +1,20 @@
+const accountId = 1445684
+let accountEmail = "imran313@gmail.accountEmail"
+var accountPassword = "12345678"
+accountCity = "jodhpur"
+
+
+// accountId = 5484448
+
+accountEmail = "hhuh@gmail.com"
+accountPassword = "8845445"
+accountCity = "jaipur"
+let accountState 
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+
+
+
