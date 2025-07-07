f7eb30d09f05f02b6c89748864411207569fb734
Devrashab <152000702+Devrashab@users.noreply.github.com>
2025-07-07 12:54:20 +0000

    conversion

--- basics/conversionoperation.js
+++ basics/conversionoperation.js
@@ -12,12 +12,10 @@
 // "33abc" => NaN
 // true => 1 ; false => 0
 
-let isLoggedIn = "Imran"
+// let isLoggedIn = "Imran"
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-//console.log(booleanIsLoggedIn);
-
-
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
 
 
@@ -26,6 +24,47 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 // "Imran" => true 
 
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
+// let someNumber = 33
+//let stringNumber = String(someNumber)
+//console.log(typeof stringNumber);
+
+
+
+//  ********************************************* Operation  **********************************************
+
+// let value = 3 
+// let nagValue = -value
+//console.log(nagValue);
+
+
+// console.log(2-2);
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+// let str1 = "Hello"
+// let str2 = " Imran"
+
+// let str3 = str1 + str2 
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 +2 + "2");
+
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1=num2=num3 = 2 + 3
+
+let gameCounter = 100
+gameCounter++ ;
+console.log(gameCounter);
