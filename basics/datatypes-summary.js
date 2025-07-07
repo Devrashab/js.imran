e6dc25f8ae4926b1214be01b63567f2b590c83ad
Devrashab <152000702+Devrashab@users.noreply.github.com>
2025-07-07 12:53:56 +0000

    conversion

---
+++ basics/datatypes-summary.js
@@ -0,0 +1,34 @@
+// #Primitive
+
+// 7 types : string, Number, Boolean, Null, undefined, Symbol, BigInt 
+
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id  = Symbol('123')
+const anotherId = Symbol('123')
+
+const bigNumber = 34565437665432n
+
+
+//Reference (Non Primitive)
+
+// Array, Objects, Function
+
+const heros =["ironmen","hulk","thor"];
+
+let myObj = {
+    name : "Imran",
+    Age : 20,
+}
+
+const myFunction = function(){
+    console.log ("Hello World ");
+}
+
+console.log(typeof bigNumber);
